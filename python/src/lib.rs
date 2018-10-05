493eb9d1e7b5d5e71f463a00cba0a12976db1b3a
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-10-05 17:44:13 +0900

    [Python] Return owned array again

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -213,9 +213,11 @@ impl GameState {
         self.prev_actions = res;
         Ok(self.state.clone())
     }
-    fn get_symbol_image(&self, state: &PlayerState) -> PyResult<&PyArray<f32>> {
+    fn get_symbol_image(&self, state: &PlayerState) -> PyResult<PyArray<f32>> {
         let py = self.token.py();
-        state.symbol_image(py)
+        state
+            .symbol_image(py)
+            .map(|arr| unsafe { arr.to_owned(py) })
     }
 }
 
