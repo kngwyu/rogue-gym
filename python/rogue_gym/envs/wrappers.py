fecdfe5cc229911ef8f60bb0a319f68fd3109fed
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-10-18 14:37:55 +0900

    [Python] Simplify wrappers

--- python/rogue_gym/envs/wrappers.py
+++ python/rogue_gym/envs/wrappers.py
@@ -2,22 +2,6 @@ from .rogue_env import PlayerState, RogueEnv
 from typing import Tuple, Union
 
 
-class FirstFloorEnv(RogueEnv):
-    def __init__(self, *args, **kwargs) -> None:
-        self.stair_reward = 100.0  # default reward
-        if 'stair_reward' in kwargs:
-            self.stair_reward = kwargs['stair_reward']
-            del kwargs['stair_reward']
-        super().__init__(*args, **kwargs)
-
-    def step(self, action: Union[int, str]) -> Tuple[PlayerState, float, bool, None]:
-        state, reward, end, _ = super().step(action)
-        if self.result.status["dungeon_level"] == 2:
-            end = True
-            reward += self.stair_reward
-        return state, reward, end, None
-
-
 class StairRewardEnv(RogueEnv):
     def __init__(self, *args, **kwargs) -> None:
         self.stair_reward = 100.0  # default reward
@@ -28,9 +12,17 @@ class StairRewardEnv(RogueEnv):
         self.current_level = 1
 
     def step(self, action: Union[int, str]) -> Tuple[PlayerState, float, bool, None]:
-        state, reward, end, _ = super().step(action)
+        state, reward, end, info = super().step(action)
         current = self.result.status["dungeon_level"]
         if self.current_level < current:
             self.current_level = current
             reward += self.stair_reward
-        return state, reward, end, None
+        return state, reward, end, info
+
+
+class FirstFloorEnv(StairRewardEnv):
+    def step(self, action: Union[int, str]) -> Tuple[PlayerState, float, bool, None]:
+        state, reward, end, info = super().step(action)
+        if self.current_level == 2:
+            end = True
+        return state, reward, end, info
