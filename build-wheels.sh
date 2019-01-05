620f001f6bb795c2d04b63bdd64b87da85aac769
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-01-05 15:09:59 +0900

    [Python] Use latest nightly in wheel build

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -ex
 
-curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly-2018-11-18  -y
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly  -y
 export PATH="$HOME/.cargo/bin:$PATH"
 cd /io/python
 
 for PYBIN in /opt/python/cp{36,37}*/bin; do
     export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
-
     "${PYBIN}/pip" install -U setuptools wheel==0.31.1 setuptools-rust
     "${PYBIN}/python" setup.py bdist_wheel
 done
--- python/README.md
+++ python/README.md
@@ -36,6 +36,6 @@ Level:  1 Gold:     0 Hp: 12(12) Str: 16(16) Arm:  0 Exp:  1/ 0
 # Developper notes
 Build wheel 
 ```
-cd rogue_gym
+cd ../
 docker run --rm -v $PWD:/io quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
 ```
